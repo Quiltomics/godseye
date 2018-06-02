23fb47de60c4b57808fbd9668922aa79a2ddc5b0
kasramvd <kasraavand@gmail.com>
2018-06-02 11:58:59 +0430

    prepare data for analyze

---
+++ test/prepare.py
@@ -0,0 +1,39 @@
+import json
+import sqlite3 as lite
+from sqlite3 import Error as LiteError
+
+
+
+class CreateDatabase:
+    def __init__(self, *args, **kwargs):
+        self.json_path = kwargs['json_path']
+        self.database_name = kwargs['database_name']
+
+    def create_connection(db_file):
+        """ create a database connection to a SQLite database """
+        try:
+            conn = sqlite3.connect(self.database_name)
+            print(sqlite3.version)
+        except LiteError as e:
+            print(e)
+        finally:
+            conn.close()
+
+    def json_parser(self):
+        with open(self.json_path) as f:
+            data = json.load(f)
+        for index, metadata in data.items():
+            article = metadata["Article"]
+            short_title = article["Journal"]["Title"]
+            title = article["ArticleTitle"]
+            abstract = article["Abstract"]["AbstractText"]
+            date = article["ArticleDate"]
+            
+    def insert(self):
+        connection = self.create_connection()
+        with connection:
+            cur = con.cursor()
+            cur.execute("create table keywords")
+            cur.execute()
+
+            rows = cur.fetchall()
