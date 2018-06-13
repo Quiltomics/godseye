d8034fe52c3891dd91cee853324b984b58924609
kasramvd <kasraavand@gmail.com>
2018-06-13 12:08:05 +0430

    Add test config file to test module

---
+++ test/test_config.py
@@ -0,0 +1,7 @@
+import os
+
+MIN_SIZE = 4  # the minimum size for a token
+FIGSIZE=(5,3)  # Figure size
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HOME_DIR = os.path.expanduser("~")
