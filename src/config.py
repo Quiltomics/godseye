4f9584c8a3cb6cb85ed7563a11c47414f5c396b3
kasramvd <kasraavand@gmail.com>
2018-07-26 14:26:33 +0430

    add new configs for updated moduels

--- src/config.py
+++ src/config.py
@@ -1,1 +1,7 @@
-MIN_SIZE = 4 # the minimum size for a token
+import os
+
+MIN_SIZE = 4  # the minimum size for a token
+FIGSIZE=(5,3)  # Figure size
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HOME_DIR = os.path.expanduser("~")
