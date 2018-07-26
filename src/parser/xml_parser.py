448a8fc34cbf419c375232a343896b08ba401005
kasramvd <kasraavand@gmail.com>
2018-07-26 14:27:49 +0430

    update parser module by using xmltodict package and fixing previous bugs

--- src/parser/xml_parser.py
+++ src/parser/xml_parser.py
@@ -1,9 +1,8 @@
 import json
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import codecs
 from xml.etree import ElementTree
-from itertools import chain
-from operator import itemgetter
+import xmltodict
 
 
 class Mainparser:
@@ -11,26 +10,39 @@ class Mainparser:
         self.json_file_name = kwargs['jfn']
         self.xml_file_name = kwargs['xfn']
 
-    def journals(self):
+    def parse_journals(self):
+        def looper(iterable):
+            for node in iterable:
+                if node.text.strip():
+                    yield (node.tag, node.text)
+                else:
+                    yield from looper(node)
+
         with open(self.xml_file_name) as f:
             tree = ElementTree.parse(f)
         root = tree.getroot()
         for n, neighbor in enumerate(root.iter('MedlineCitation')):
-            
-            sub = {node.tag: {node.tag: node.text}
-                    if node.text != '\n' 
-                    else {el.tag: el.text if el.text != '\n'
-                                          else {e.tag: e.text for e in el}
-                    for el in node.getchildren()}
-                    for node in neighbor.getchildren()}
-            
-            if neighbor.text:
-                yield n, sub
+            d = defaultdict(list)
+            for i, j in looper(neighbor.getchildren()):
+                d[i].append(j)
+            yield n, d
 
     def create_dict(self):
-        return OrderedDict(self.journals())
+        return OrderedDict(self.parse_journals())
 
-    def create_json(self):
-        the = self.create_dict()
+    def create_json(self, dictionary):
         with codecs.open(self.json_file_name, 'w', encoding='UTF-8') as f:
-            json.dump(the, f, indent=4)
+            json.dump(dictionary, f, indent=4)
+
+    def xml_to_json(self):
+        with open(self.xml_file_name) as f:
+            d = xmltodict.parse(f.read())
+        return d
+
+if __name__ == '__main__':
+    MP = Mainparser(xfn='../godseye-files/pubmed18n0001.xml',
+                    jfn='../godseye-files/articles.json')
+
+    # d = MP.create_dict()
+    d = MP.xml_to_json()
+    MP.create_json(d)
