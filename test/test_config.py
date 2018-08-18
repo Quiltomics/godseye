b212fadc3954d52825b233352f59c7ff38abf22f
Kasramvd <kasraavand@gmail.com>
2018-08-18 15:46:13 +0430

    add predifined keywords to config file

--- test/test_config.py
+++ test/test_config.py
@@ -1,7 +1,19 @@
 import os
 
 MIN_SIZE = 4  # the minimum size for a token
-FIGSIZE=(5,3)  # Figure size
+FIGSIZE=(5, 3)  # Figure size
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 HOME_DIR = os.path.expanduser("~")
+
+KEYWORDS = (
+    "coronary artery",
+    "congestive heart",
+    "congenital heart",
+    "hypertension",
+    "cardiomyopathy",
+    "stroke",
+    "myocardial infarction",
+    "angina",
+    "arrhythmia",
+)
