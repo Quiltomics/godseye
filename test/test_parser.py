4be87d8749d24bee296e54317df3c964114757b3
Kasramvd <kasraavand@gmail.com>
2018-08-25 17:41:12 +0430

    update parser to iteratively parse xml files

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,12 +3,15 @@ from collections import OrderedDict, defaultdict
 import codecs
 from xml.etree import ElementTree
 import xmltodict
+from os import path as ospath
+import test_config
+
 
 
 class Mainparser:
     def __init__(self, *args, **kwargs):
-        self.json_file_name = kwargs['jfn']
-        self.xml_file_name = kwargs['xfn']
+        self.json_path = kwargs['json_path']
+        self.xml_path = kwargs['xml_path']
 
     def parse_journals(self):
         def looper(iterable):
@@ -30,18 +33,23 @@ class Mainparser:
     def create_dict(self):
         return OrderedDict(self.parse_journals())
 
-    def create_json(self, dictionary):
-        with codecs.open(self.json_file_name, 'w', encoding='UTF-8') as f:
-            json.dump(dictionary, f, indent=4)
+    def create_jsons(self):
+        for dictionary in self.xml_to_json():
+            with codecs.open(self.json_path, 'w', encoding='UTF-8') as f:
+                json.dump(dictionary, f, indent=4)
 
     def xml_to_json(self):
-        with open(self.xml_file_name) as f:
-            d = xmltodict.parse(f.read())
-        return d
+        for name in glob.glob(self.xml_path):
+            with open(name) as f:
+                d = xmltodict.parse(f.read())
+            yield d
+    
+    def run(self):
+
 
 if __name__ == '__main__':
-    MP = Mainparser(xfn='../godseye-files/pubmed18n0001.xml',
-                    jfn='../godseye-files/articles.json')
+    MP = Mainparser(xfn=ospath.join(test_config.HOME_DIR, 'godseye-files/pubmed18n0001.xml'),
+                    jfn=ospath.join(test_config.HOME_DIR, 'godseye-files/articles.json'))
 
     # d = MP.create_dict()
     d = MP.xml_to_json()
