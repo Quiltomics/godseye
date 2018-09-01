790e2bce0965bb5c8e5ea8f0d9464252bd25c28e
Kasramvd <kasraavand@gmail.com>
2018-09-01 11:38:03 +0430

    convert godseye to a django-app

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-MIN_SIZE = 4  # the minimum size for a token
-FIGSIZE=(5,3)  # Figure size
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-HOME_DIR = os.path.expanduser("~")
